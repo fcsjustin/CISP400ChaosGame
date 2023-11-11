1654d26fd21f98698a3a8368155cc091b4c7ee61
Your Name <you@example.com>
2023-11-11 13:51:43 -0800

    chaos game

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -18,6 +18,29 @@ int main()
     vector<Vector2f> points;
     srand(static_cast<unsigned>(time(NULL)));
 
+    Font font;
+    if (!font.loadFromFile("arial.ttf"))
+    {
+        cerr << "Error loading font." << endl;
+        return -1;
+    }
+
+    Text instructionText;
+    instructionText.setFont(font);
+    instructionText.setCharacterSize(30);
+    instructionText.setFillColor(Color::White);
+    instructionText.setPosition(10, 10);
+    instructionText.setString("Click on any three points to create the vertices for the triangle.");
+
+    Text algorithmText;
+    algorithmText.setFont(font);
+    algorithmText.setCharacterSize(30);
+    algorithmText.setFillColor(Color::White);
+    algorithmText.setPosition(10, 50);
+
+    bool algorithmStarted = false;
+    Vector2f startCoordinate;
+
     while (window.isOpen())
     {
         Event event;
@@ -29,20 +52,31 @@ int main()
             }
             if (event.type == Event::MouseButtonPressed)
             {
-                if (event.mouseButton.button == Mouse::Left)
+                if (!algorithmStarted)
                 {
-                    cout << "the left button was pressed" << endl;
-                    cout << "mouse x: " << event.mouseButton.x << endl;
-                    cout << "mouse y: " << event.mouseButton.y << endl;
-
-                    if (vertices.size() < 3)
+                    if (event.mouseButton.button == Mouse::Left)
                     {
-                        vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                        if (vertices.size() < 3)
+                        {
+                            vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                        }
+                        else
+                        {
+                            algorithmStarted = true;
+                            startCoordinate = Vector2f(event.mouseButton.x, event.mouseButton.y);
+                            algorithmText.setString("Algorithm started. Click to generate points.");
+                        }
                     }
-                    else if (points.empty())
-                    {
-                        // Fourth click, push back to points vector
-                        points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                }
+                else
+                {
+                    for (int i = 0; i < 25; i++)
+                    {   
+                    int randomVertexIndex = rand() % vertices.size();
+                    Vector2f lastPoint = points.empty() ? startCoordinate : points.back();
+                    Vector2f selectedVertex = vertices[randomVertexIndex];
+                    Vector2f midpoint = (lastPoint + selectedVertex) / 2.0f;
+                    points.push_back(midpoint);
                     }
                 }
             }
@@ -53,34 +87,26 @@ int main()
             window.close();
         }
 
-        if (points.size() > 0)
-        {
-            // Generate more points within the triangle
-            int randomVertexIndex = rand() % 3; // Randomly select one of the three vertices
-            Vector2f lastPoint = points.back();
-            Vector2f selectedVertex = vertices[randomVertexIndex];
-            Vector2f midpoint = (lastPoint + selectedVertex) / 2.0f;
-            points.push_back(midpoint);
-        }
-
         window.clear();
 
         for (size_t i = 0; i < vertices.size(); i++)
         {
-            RectangleShape rect(Vector2f(10, 10));
-            rect.setPosition(vertices[i]);
-            rect.setFillColor(Color::Blue);
-            window.draw(rect);
+            CircleShape vertex(5);
+            vertex.setPosition(vertices[i]);
+            vertex.setFillColor(Color::Blue);
+            window.draw(vertex);
         }
 
         for (size_t i = 0; i < points.size(); i++)
         {
-            CircleShape circle(5);
-            circle.setPosition(points[i]);
-            circle.setFillColor(Color::Red);
-            window.draw(circle);
+            CircleShape point(2);
+            point.setPosition(points[i]);
+            point.setFillColor(Color::Red);
+            window.draw(point);
         }
 
+        window.draw(instructionText);
+        window.draw(algorithmText);
         window.display();
     }
 
